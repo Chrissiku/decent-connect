c76ecb05184af09b869559a8e5c62727b0ef7613
ginabeki <ginabeki3@gmail.com>
2023-12-28 21:43:28 +0300

    Render component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Web5 } from '@web5/api/browser';
+import HowItWorks from './components/HowItWorks';
 console.log(Web5)
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-800">
-      Hello world!
-    </h1>
+    <>
+      <div>
+    <HowItWorks />
+      </div>
+    </>
   )
 }
