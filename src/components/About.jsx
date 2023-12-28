4bfa331c0559007c5bf6a58156f8736afa387a6a
Chris Siku <chrissiku5@gmail.com>
2023-12-28 19:48:50 +0200

    Install Swiperjs

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <div className="w-full mx-auto">
+      <div className="grid grid-cols-1 lg:grid-cols-2 items-center justify-center">
+        <div>Grid 1</div>
+        <div>Grid 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import About from "../components/About";
 import Header from "../components/Header";
 
 const Home = () => {
   return (
     <div className="w-full flex flex-col mx-auto items-center justify-start">
       <Header />
+      <About />
     </div>
   );
 };
