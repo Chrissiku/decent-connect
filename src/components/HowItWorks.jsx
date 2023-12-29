c501a3a89355f7895e00e1708432445530487cbe
ginabeki <ginabeki3@gmail.com>
2023-12-29 07:41:47 +0300

    Make how it works section responsive

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -5,8 +5,9 @@ const HowItWorks = () => {
     <div className='mx-auto w-[75%] mt-[100px]'>
         <h2 className='text-center font-semibold text-3xl mb-3'>How it Works</h2>
         <p className='text-center mb-10'>Step-by-step guide on how the platform works</p>
-        <img src={steps} alt='Step by step bar'/>
-        <div className='flex gap-x-12'>
+        <div className='flex md:flex-col'>
+        <img className='rotate-90 md:rotate-0 w-1/4 md:w-full' src={steps} alt='Step by step bar'/>
+        <div className='flex w-3/4 flex-col gap-x-12 md:flex-row'>
             <div>
             <h4 className='text-[#17A398] text-xl'>Connect</h4>
             <p>Find a mentor in your desired profession</p>
@@ -19,6 +20,7 @@ const HowItWorks = () => {
                 <h4 className='text-[#17A398] text-xl'>Confirm and join</h4>
                 <p>Confirm the session details and join the video call</p>
             </div>
+        </div>
         </div>
     </div>
   )
