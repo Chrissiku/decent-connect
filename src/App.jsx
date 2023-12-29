bad116af24bf1976a199d219c6fb9c92f2e298b3
Chris Siku <chrissiku5@gmail.com>
2023-12-29 14:09:24 +0200

    Update the Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import Home from "./pages/Home";
-import { Web5 } from '@web5/api/browser';
-import HowItWorks from './components/HowItWorks';
-import Mentor from './components/Mentor';
-import Journey from './components/Journey';
-import Footer from './components/Footer';
-console.log(Web5)
 export default function App() {
   return (
     <>
       <div>
-         <div className="w-full mx-auto flex flex-col items-center justify-center">
-        <Home />
-      </div>
-    <HowItWorks />
-    <Mentor />
-    <Journey />
-    <Footer />
+        <div className="w-full mx-auto flex flex-col">
+          <Home />
+        </div>
       </div>
     </>
-  )
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,24 @@
 import About from "../components/About";
 import Experience from "../components/Experience";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
+import HowItWorks from "../components/HowItWorks";
+import Journey from "../components/Journey";
+import Mentor from "../components/Mentor";
 
 const Home = () => {
   return (
-    <div className="w-full flex flex-col mx-auto items-center justify-start">
-      <Header />
-      <About />
-      <Experience />
-    </div>
+    <>
+      <div className="w-full flex flex-col mx-auto">
+        <Header />
+        <About />
+        <Experience />
+        <HowItWorks />
+        <Mentor />
+        <Journey />
+        <Footer />
+      </div>
+    </>
   );
 };
 
