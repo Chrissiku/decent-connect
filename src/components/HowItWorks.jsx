0a9cf5e14d9ddaef1982ae3827eb7166306436f0
ginabeki <ginabeki3@gmail.com>
2023-12-29 10:00:48 +0300

    Create section

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -3,11 +3,11 @@ import steps from '../assets/steps.svg'
 const HowItWorks = () => {
   return (
     <div className='mx-auto w-[75%] mt-[100px]'>
-        <h2 className='text-center font-semibold text-3xl mb-3'>How it Works</h2>
+        <h2 className='text-center font-semibold text-2xl md:text-3xl mb-3'>How it Works</h2>
         <p className='text-center mb-10'>Step-by-step guide on how the platform works</p>
         <div className='flex md:flex-col'>
-        <img className='rotate-90 md:rotate-0' src={steps} alt='Step by step bar'/>
-        <div className='flex flex-col gap-x-12 md:flex-row'>
+        <img className='hidden md:block md:rotate-0' src={steps} alt='Step by step bar'/>
+        <div className='flex flex-col gap-y-12 gap-x-12 md:gap-y-0 md:flex-row'>
             <div>
             <h4 className='text-[#17A398] text-xl'>Connect</h4>
             <p>Find a mentor in your desired profession</p>
