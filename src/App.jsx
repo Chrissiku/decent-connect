3e5cfedd2c6abe26abcc186394e4b5ca87ea2e09
ginabeki <ginabeki3@gmail.com>
2023-12-29 08:33:30 +0300

    Render Journey component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Web5 } from '@web5/api/browser';
 import HowItWorks from './components/HowItWorks';
+import Mentor from './components/Mentor';
+import Journey from './components/Journey';
 console.log(Web5)
 export default function App() {
   return (
     <>
       <div>
     <HowItWorks />
+    <Mentor />
+    <Journey />
       </div>
     </>
   )
