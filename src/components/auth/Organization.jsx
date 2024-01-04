82d55726b98ea122c7abb079dc3933332d36b22d
Chris Siku <chrissiku5@gmail.com>
2024-01-04 17:08:23 +0200

    Change button text

--- src/components/auth/Organization.jsx
+++ src/components/auth/Organization.jsx
@@ -114,7 +114,7 @@ const Organization = () => {
                 type="button"
                 className="w-full text-white bg-teal hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               >
-                Create an account
+                Create an Organization
               </button>
             </form>
           </div>
