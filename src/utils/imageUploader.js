d6b69eda141d204f9923920ea0e0def9f709d86b
Chris Siku <chrissiku5@gmail.com>
2024-01-05 15:08:33 +0200

    Update imageUploader hook

--- src/utils/imageUploader.js
+++ src/utils/imageUploader.js
@@ -7,7 +7,18 @@ const useImageUploader = () => {
     const inputField = event.target;
     const [file] = inputField.files;
     if (file) {
-      setPicture(URL.createObjectURL(file));
+      const fileSize = file.size / 1024;
+      if (fileSize > 700) {
+        event.target.value = null;
+        alert("Please select an image with a size less than 700KB.");
+      } else {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const imageDataUrl = e.target.result;
+          setPicture(imageDataUrl);
+        };
+        reader.readAsDataURL(file);
+      }
     }
   };
   return {
