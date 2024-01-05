e3c5d889de44f13a0c186726e4915e997d0166e9
ginabeki <ginabeki3@gmail.com>
2024-01-05 18:48:31 +0300

    Add route for therapist dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PatientDashboard from "./pages/PatientDashboard";
+import TherapistDashboard from "./pages/TherapistDashboard";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/patient-dashboard" element={<PatientDashboard />} />
+        <Route path="/therapist-dashboard" element={< TherapistDashboard />} />
         </Routes>
       </BrowserRouter>
     </>
