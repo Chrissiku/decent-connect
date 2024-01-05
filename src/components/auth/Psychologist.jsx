2cde903d08451e54106acfa5d1a4fe2033f34de3
Chris Siku <chrissiku5@gmail.com>
2024-01-05 19:17:17 +0200

    Implement psychologist authentication logic

--- src/components/auth/Psychologist.jsx
+++ src/components/auth/Psychologist.jsx
@@ -1,10 +1,85 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import psychologistImg from "../../assets/psychologistAuth.jpg";
 import { AppContext } from "../../context/ContextProvider";
+import useImageUploader from "../../utils/imageUploader";
+import { v4 as uidv4 } from "uuid";
 
 const Psychologist = () => {
-  const { organizationList } = useContext(AppContext);
-  return (  
+  const {
+    web5,
+    did,
+    protocolDefinition,
+    toggleUserType,
+    togglePsy,
+    organizationList,
+    publicDid,
+  } = useContext(AppContext);
+
+  const { picture: profile, handleImageChange } = useImageUploader();
+  const [name, setName] = useState("");
+  const [experience, setExperience] = useState(1);
+  const [gender, setGender] = useState("");
+  const [specialization, setSpecialization] = useState("");
+  const [organization, setOrganization] = useState("");
+
+  const formData = {
+    id: uidv4(),
+    name,
+    profile,
+    experience,
+    gender,
+    specialization,
+    organization,
+  };
+
+  const createPsy = async (event) => {
+    event.preventDefault();
+    if (
+      !formData.name ||
+      !formData.profile ||
+      !formData.experience ||
+      !formData.gender ||
+      !formData.specialization
+    ) {
+      alert("Please fill in all the required fields !");
+    } else {
+      try {
+        const { record, status } = await web5.dwn.records.create({
+          data: formData,
+          message: {
+            protocol: protocolDefinition.protocol,
+            protocolPath: "psychologistProfile",
+            schema: protocolDefinition.types.psychologistProfile.schema,
+            recipient: did,
+            published: true,
+          },
+        });
+
+        const DIDs = [did, publicDid];
+        await Promise.all(
+          DIDs.map(async (did) => {
+            await record.send(did);
+          })
+        );
+
+        if (status.code === 202 && status.detail === "Accepted") {
+          setName("");
+          setGender("");
+          setExperience(1);
+          setOrganization("");
+          setOrganization("");
+          toggleUserType("psychologist");
+          togglePsy(true);
+        }
+      } catch (error) {
+        console.error("Error Creating this profile : ", error);
+      }
+    }
+  };
+
+  console.log(formData);
+
+  return (
     <>
       <section className="w-full rounded-lg">
         <div className="w-full mx-auto grid grid-cols-1 lg:grid-cols-2 items-center justify-center gap-5">
@@ -28,7 +103,12 @@ const Psychologist = () => {
               Join our mental health community to support and connect with those
               in need.
             </h3>
-            <form className="space-y-4 md:space-y-6" autoComplete="off">
+            <form
+              className="space-y-4 md:space-y-6"
+              autoComplete="off"
+              onSubmit={createPsy}
+            >
+              {/* Name */}
               <div>
                 <label
                   htmlFor="name"
@@ -40,12 +120,15 @@ const Psychologist = () => {
                   type="text"
                   name="name"
                   id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-3"
                   placeholder="eg. John Doe"
-                  required=""
+                  required
                 />
               </div>
               <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-between gap-5">
+                {/* Profile */}
                 <div>
                   <label
                     htmlFor="profile"
@@ -57,11 +140,13 @@ const Psychologist = () => {
                     type="file"
                     name="profile"
                     id="profile"
+                    onChange={handleImageChange}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-3"
                     placeholder="eg. John Doe"
-                    required=""
+                    required
                   />
                 </div>
+                {/* experience */}
                 <div>
                   <label
                     htmlFor="experience"
@@ -74,13 +159,16 @@ const Psychologist = () => {
                     type="number"
                     name="experience"
                     id="experience"
+                    value={experience}
+                    onChange={(e) => setExperience(e.target.value)}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-3"
                     placeholder="Your Date of birth here"
-                    required=""
+                    required
                   />
                 </div>
               </div>
               <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-between gap-5">
+                {/* Gender */}
                 <div>
                   <label
                     htmlFor="gender"
@@ -90,61 +178,85 @@ const Psychologist = () => {
                   </label>
                   <select
                     id="gender"
+                    name="gender"
+                    value={gender}
+                    onChange={(e) => setGender(e.target.value)}
+                    required
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-3 "
                   >
+                    <option defaultValue="">...</option>
                     <option defaultValue="female">Female</option>
                     <option value="male">Male</option>
                     <option value="other">Other</option>
                   </select>
                 </div>
+                {/* Specialization */}
                 <div>
                   <label
-                    htmlFor="gender"
+                    htmlFor="specialization"
                     className="block mb-2 text-[16px] font-medium text-gray-900"
                   >
                     Select Specialization{" "}
                     <span className="text-red-500">*</span>
                   </label>
                   <select
-                    id="gender"
+                    id="specialization"
+                    name="specialization"
+                    value={specialization}
+                    onChange={(e) => setSpecialization(e.target.value)}
+                    required
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-3 "
                   >
                     <option defaultValue="generalist">Generalist</option>
                     <option value="Clinical">Clinical Psychologist</option>
-                    <option value="">Counseling Psychologist</option>
-                    <option value="">School Psychologist</option>
-                    <option value="">Forensic Psychologist</option>
-                    <option value="">
+                    <option value="Counseling">Counseling Psychologist</option>
+                    <option value="School">School Psychologist</option>
+                    <option value="Forensic">Forensic Psychologist</option>
+                    <option value="Industrial-Organizational">
                       Industrial-Organizational Psychologist
                     </option>
-                    <option value="">Neuropsychologist</option>
-                    <option value="">Sports Psychologist</option>
-                    <option value="">Health Psychologist</option>
-                    <option value="">Developmental Psychologist</option>
-                    <option value="">Social Psychologist</option>
-                    <option value="">Experimental Psychologist</option>
-                    <option value="">Cognitive Psychologist</option>
-                    <option value="">Educational Psychologist</option>
-                    <option value="">Geriatric Psychologist</option>
-                    <option value="">Child Psychologist</option>
-                    <option value="">Environmental Psychologist</option>
-                    <option value="">Rehabilitation Psychologist</option>
-                    <option value="">Cross Psychologist-Cultural</option>
-                    <option value="">Positive Psychologist</option>
-                    <option value="">Media Psychologist Psychologist</option>
+                    <option value="Neuropsychologist">Neuropsychologist</option>
+                    <option value="Sports">Sports Psychologist</option>
+                    <option value="Health">Health Psychologist</option>
+                    <option value="Developmental">
+                      Developmental Psychologist
+                    </option>
+                    <option value="Social">Social Psychologist</option>
+                    <option value="Experimental">
+                      Experimental Psychologist
+                    </option>
+                    <option value="Cognitive">Cognitive Psychologist</option>
+                    <option value="Educational">
+                      Educational Psychologist
+                    </option>
+                    <option value="Geriatric">Geriatric Psychologist</option>
+                    <option value="Child">Child Psychologist</option>
+                    <option value="Environmental">
+                      Environmental Psychologist
+                    </option>
+                    <option value="Rehabilitation">
+                      Rehabilitation Psychologist
+                    </option>
+                    <option value="Cross">Cross Psychologist-Cultural</option>
+                    <option value="Positive">Positive Psychologist</option>
+                    <option value="Media">Media Psychologist</option>
                   </select>
                 </div>
               </div>
+              {/* Organization */}
               <div>
                 <label
-                  htmlFor="gender"
+                  htmlFor="organization"
                   className="block mb-2 text-[16px] font-medium text-gray-900"
                 >
                   Select Your Organization{" "}
                   <span className="text-red-300">(Optional)</span>
                 </label>
                 <select
-                  id="gender"
+                  id="organization"
+                  name="organization"
+                  value={organization}
+                  onChange={(e) => setOrganization(e.target.value)}
                   className="capitalize bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-3 "
                 >
                   <option defaultValue="self employed">Self Employed</option>
@@ -156,7 +268,7 @@ const Psychologist = () => {
                 </select>
               </div>
               <button
-                type="button"
+                type="submit"
                 className="w-full text-white bg-teal hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               >
                 Create an account
