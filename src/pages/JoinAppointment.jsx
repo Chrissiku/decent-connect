ea4e7df84210dfba5f59e2e3ba7016ae557c4f5c
Chris Siku <chrissiku5@gmail.com>
2024-01-10 11:12:45 +0200

    Add close button to the UI

--- src/pages/JoinAppointment.jsx
+++ src/pages/JoinAppointment.jsx
@@ -1,6 +1,6 @@
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
-import React, { useContext, useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { AppContext } from "../context/ContextProvider";
 import { v4 as uid } from "uuid";
 
@@ -12,12 +12,11 @@ const JoinAppointment = () => {
   useEffect(() => {
     if (did) {
       setUser(did);
-      setUserName("You Name");
+      setUserName("Anonymous");
     }
   }, [did]);
 
   const params = useParams();
-  const navigate = useNavigate();
 
   const handleGoBack = () => {
     window.close();
@@ -29,7 +28,7 @@ const JoinAppointment = () => {
       import.meta.env.VITE_ZEGOCLOUD_SERVER_SECRET,
       params.id,
       user ? user : uid(),
-      userName ? userName : "Your Name"
+      userName ? userName : "Anonymous"
     );
     const zp = ZegoUIKitPrebuilt.create(kitToken);
 
@@ -49,7 +48,7 @@ const JoinAppointment = () => {
   };
 
   return (
-    <div className="bg-light-gray w-full mx-auto grid grid-cols-1 items-center justify-center">
+    <div className="relative bg-light-gray w-full mx-auto grid grid-cols-1 items-center justify-center">
       <div
         style={{
           height: "100vh",
@@ -59,13 +58,17 @@ const JoinAppointment = () => {
         <div className="mx-auto w-[200px]">
           <button
             type="button"
-            className="bg-teal px-5 py-3 text-white self-center w-full rounded-lg"
+            className="bg-teal px-5 py-2 text-white self-center w-full rounded-lg z-50"
             onClick={handleGoBack}
           >
             Close
           </button>
         </div>
-        <div className="myCallContainer w-full h-full" ref={myMeeting}></div>
+        <div
+          className="myCallContainer"
+          style={{ width: "100%", height: "90vh" }}
+          ref={myMeeting}
+        ></div>
       </div>
     </div>
   );
