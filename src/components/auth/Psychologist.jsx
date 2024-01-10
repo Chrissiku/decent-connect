4f5a58b233139e5fd8d393553cee4e9f0f9c1656
Chris Siku <chrissiku5@gmail.com>
2024-01-10 17:01:12 +0200

    Implement the VC

--- src/components/auth/Psychologist.jsx
+++ src/components/auth/Psychologist.jsx
@@ -217,7 +217,7 @@ const Psychologist = () => {
                       required
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-3 "
                     >
-                      <option defaultValue="generalist">Generalist</option>
+                      <option defaultValue="generalist" value="Generalist">Generalist</option>
                       <option value="Clinical">Clinical Psychologist</option>
                       <option value="Counseling">
                         Counseling Psychologist
--- src/pages/Organization.jsx
+++ src/pages/Organization.jsx
@@ -3,9 +3,17 @@ import { AppContext } from "../context/ContextProvider";
 import Loader from "../components/common/Loader";
 
 const Organization = () => {
-  const { web5, did, logout, protocolDefinition } = useContext(AppContext);
+  const {
+    web5,
+    did,
+    logout,
+    protocolDefinition,
+    psychologistList,
+    toggleModalContent,
+    setCustomModalOpen,
+  } = useContext(AppContext);
   const [organizationInfo, setOrganizationInfo] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useState(() => {
     const fetchData = async () => {
@@ -37,22 +45,30 @@ const Organization = () => {
     };
 
     if (web5 && did) {
-      fetchData();
       setLoading(false);
+      fetchData();
     }
   }, [web5, did, protocolDefinition]);
+
+  const handleIssueVC = () => {
+    toggleModalContent("issue-vc");
+    setCustomModalOpen(true);
+  };
+
+  // console.log(organizationInfo);
+  // console.log(psychologistList);
   return (
     <>
-      {loading ? (
+      {!loading ? (
         <Loader />
       ) : (
         <>
           {Object.keys(organizationInfo).length === 0 ? (
             <Loader />
           ) : (
-            <div className="flex justify-between p-4 bg-[#F7F6FE] h-screen">
-              <div className="flex flex-col gap-4">
-                <div className="bg-white p-4 w-80 h-80 border flex flex-col shadow-md gap-10 rounded-[8px]">
+            <div className="grid grid-cols-1 lg:grid-cols-9 gap-3 justify-between p-4 bg-[#F7F6FE] h-screen">
+              <div className="lg:col-span-2 flex flex-col gap-4">
+                <div className="w-full bg-white p-4 h-[300px] flex flex-col shadow-md gap-10 rounded-[8px]">
                   <div className="flex flex-col">
                     <h3 className="font-semibold text-base">
                       Organization Name
@@ -85,18 +101,61 @@ const Organization = () => {
                   Logout
                 </button>
               </div>
-              <div className="font-semibold text-base">
-                List of Accredited Therapists{" "}
-                <span className="bg-[#8B7EF8] h-10 w-16 p-1 rounded-[8px] text-white">
-                  00
-                </span>
-              </div>
-              <div>
-                <img
-                  className="h-10 w-10 rounded-full"
-                  src={organizationInfo.logo}
-                  alt={organizationInfo.name}
-                />
+              {/* Content */}
+              <div className="lg:col-span-7 w-full flex flex-col items-start justify-start">
+                <div className="mx-auto text-center font-semibold text-base">
+                  List of Accredited Therapists{" "}
+                  <span className="bg-[#8B7EF8] h-10 w-16 p-1 rounded-[8px] text-white">
+                    {psychologistList.length}
+                  </span>
+                </div>
+                <div className="w-full p-5 overflow-auto">
+                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+                    {psychologistList.map((item, index) => (
+                      <div
+                        key={index}
+                        className="h-auto flex flex-col items-start justify-between space-y-3 pb-2 
+                        rounded-xl drop-shadow bg-slate-200 hover:bg-slate-300 scale-95 transition-all duration-300 hover:scale-100 overflow-hidden"
+                      >
+                        <div className="w-full h-[150px]">
+                          <img
+                            src="https://img.freepik.com/free-photo/mental-health-care-sketch-diagram_53876-121351.jpg?w=1480&t=st=1703803875~exp=1703804475~hmac=887fb9b2d5b127a8e87c4594fbe547e97cd1154944437727ad132b75f00e0d77"
+                            alt="therapist Profile"
+                            className="w-full h-full object-cover"
+                          />
+                        </div>
+                        <div
+                          className="w-full flex flex-col items-start justify-between space-y-3 
+                        p-2 text-[12px] capitalize"
+                        >
+                          <table className="w-full text-left">
+                            <tr>
+                              <th className="text-gray-500">Name : </th>
+                              <td>{item.name} </td>
+                            </tr>
+                            <tr>
+                              <th className="text-gray-500">Spec : </th>
+                              <td>{item.specialization}</td>
+                            </tr>
+                            <tr>
+                              <th className="text-gray-500">Experience : </th>
+                              <td>
+                                {item.experience} year
+                                {item.experience > 1 && "s"}
+                              </td>
+                            </tr>
+                          </table>
+                          <button
+                            onClick={() => handleIssueVC()}
+                            className="text-center text-white text-[13px] w-full bg-teal hover:bg-indigo-500 p-3 rounded-lg"
+                          >
+                            Issue V. Credential
+                          </button>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
               </div>
             </div>
           )}
