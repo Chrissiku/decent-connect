aff8f35d0b80c4e46065a1cdd1f3424fa4f4d28e
Chris Siku <chrissiku5@gmail.com>
2024-01-10 13:54:44 +0200

    Add loader to Organization dashboard

--- src/pages/Organization.jsx
+++ src/pages/Organization.jsx
@@ -41,58 +41,66 @@ const Organization = () => {
       setLoading(false);
     }
   }, [web5, did, protocolDefinition]);
-  // console.log(organizationInfo);
   return (
     <>
       {loading ? (
         <Loader />
       ) : (
-        <div className="flex justify-between p-4 bg-[#F7F6FE] h-screen">
-          {/* <div>{organizationInfo.id}</div> */}
-          <div className="flex flex-col gap-4">
-            <div className="bg-white p-4 w-80 h-80 border flex flex-col shadow-md gap-10 rounded-[8px]">
-              <div className="flex flex-col">
-                <h3 className="font-semibold text-base">Organization Name</h3>
-                <span className="text-[#9C9A9A] text-sm">
-                  {organizationInfo.name}
+        <>
+          {Object.keys(organizationInfo).length === 0 ? (
+            <Loader />
+          ) : (
+            <div className="flex justify-between p-4 bg-[#F7F6FE] h-screen">
+              <div className="flex flex-col gap-4">
+                <div className="bg-white p-4 w-80 h-80 border flex flex-col shadow-md gap-10 rounded-[8px]">
+                  <div className="flex flex-col">
+                    <h3 className="font-semibold text-base">
+                      Organization Name
+                    </h3>
+                    <span className="text-[#9C9A9A] text-sm">
+                      {organizationInfo.name}
+                    </span>
+                  </div>
+                  <div className="flex flex-col">
+                    <h3 className="font-semibold text-base">
+                      Established Date
+                    </h3>
+                    <span className="text-[#9C9A9A] text-sm">
+                      {organizationInfo.creationDate}
+                    </span>
+                  </div>
+                  <div className="flex flex-col">
+                    <h3 className="font-semibold text-base">Street Address</h3>
+                    <span className="text-[#9C9A9A] text-sm">
+                      {organizationInfo.address}
+                    </span>
+                  </div>
+                  <div>{organizationInfo.description}</div>
+                </div>
+                <button
+                  type="button"
+                  className="bg-[#8B7EF8] text-white w-[100px] font-semibold h-8 mt-40 rounded-[8px]"
+                  onClick={logout}
+                >
+                  Logout
+                </button>
+              </div>
+              <div className="font-semibold text-base">
+                List of Accredited Therapists{" "}
+                <span className="bg-[#8B7EF8] h-10 w-16 p-1 rounded-[8px] text-white">
+                  00
                 </span>
               </div>
-              <div className="flex flex-col">
-                <h3 className="font-semibold text-base">Established Date</h3>
-                <span className="text-[#9C9A9A] text-sm">
-                  {organizationInfo.creationDate}
-                </span>
+              <div>
+                <img
+                  className="h-10 w-10 rounded-full"
+                  src={organizationInfo.logo}
+                  alt={organizationInfo.name}
+                />
               </div>
-              <div className="flex flex-col">
-                <h3 className="font-semibold text-base">Street Address</h3>
-                <span className="text-[#9C9A9A] text-sm">
-                  {organizationInfo.address}
-                </span>
-              </div>
-              <div>{organizationInfo.description}</div>
             </div>
-            <button
-              type="button"
-              className="bg-[#8B7EF8] text-white w-[100px] font-semibold h-8 mt-40 rounded-[8px]"
-              onClick={logout}
-            >
-              Logout
-            </button>
-          </div>
-          <div className="font-semibold text-base">
-            List of Accredited Therapists{" "}
-            <span className="bg-[#8B7EF8] h-10 w-16 p-1 rounded-[8px] text-white">
-              00
-            </span>
-          </div>
-          <div>
-            <img
-              className="h-10 w-10 rounded-full"
-              src={organizationInfo.logo}
-              alt={organizationInfo.name}
-            />
-          </div>
-        </div>
+          )}
+        </>
       )}
     </>
   );
