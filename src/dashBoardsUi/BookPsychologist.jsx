9238032b986d953d3561e120b6c6b7f7547a86d0
ginabeki <ginabeki3@gmail.com>
2024-01-11 17:28:33 +0300

    Fix typo

--- src/dashBoardsUi/BookPsychologist.jsx
+++ src/dashBoardsUi/BookPsychologist.jsx
@@ -79,7 +79,7 @@ const BookPsychologist = () => {
       ) : (
         <div className="md:p-5 grid grid-cols-1 items-center justify-center space-y-5">
           <h3 className="text-teal font-bold text-[20px]">
-            Complete this form to schedule an Meeting.
+            Complete this form to schedule a Meeting.
           </h3>
           <form
             className="space-y-3 md:space-y-4"
